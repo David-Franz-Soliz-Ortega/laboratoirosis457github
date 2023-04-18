47223585c3715b2aa35dc274d97647b8780a5512
David-Franz-Soliz-Ortega <114242008+David-Franz-Soliz-Ortega@users.noreply.github.com>
2023-04-18 11:42:33 -0400

    Add files via upload

---
+++ laboratorio/factorial.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<stdlib.h>
+using namespace std;
+
+
+class factorial{
+private:
+	int n;
+public:
+	factorial(int _n){
+		n=_n;
+	}
+	void calcular(){
+		int result=1;
+		for(int i=1;i<=n;i++){
+			result=result*i;
+		}
+		cout<<result<<endl;
+	};
+};
+
+int main(){
+	factorial o1(3);
+	factorial o2(4);
+	factorial o3(5);
+	o1.calcular();
+	o2.calcular();
+	o3.calcular();
+}
